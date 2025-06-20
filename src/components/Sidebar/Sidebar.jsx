41c82bf55bace58a39ca2303a1966ff36f396ca3
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-06-20 15:56:40 +0300

    Add new component Routes, corrections

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,21 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../../assets/logo.png';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-// import Title from '../Title/Title';
-
-const routes = [
-    { title: 'Home', icon: 'fas-solid fa-house', path: '/' },
-    { title: 'Sales', icon: 'chart-line', path: '/sales' },
-    { title: 'Costs', icon: 'chart-column', path: '/costs' },
-    { title: 'Payments', icon: 'wallet', path: '/payments' },
-    { title: 'Finances', icon: 'chart-pie', path: '/finances' },
-    { title: 'Messages', icon: 'envelope', path: '/messages' },
-];
-
-const bottomRoutes = [
-    { title: 'Settings', icon: 'sliders', path: '/settings' },
-    { title: 'Support', icon: 'phone-volume', path: '/support' },
-];
+import Routes from '../Routes/Routes';
 
 //Sidebar
 const StyledSidebar = styled.div`
@@ -71,46 +57,10 @@ const StyledToggle = styled.div`
 	z-index: 10;
 `;
 
-
-//routes
-const StyledRoutesWrapper = styled.div`
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: stretch;
-`;
-
-const StyledRoute = styled.div`
-	padding: 0.7em;
-	color: var(--color-text-light-default);
-	background-color: var(--color-sidebar-background-light-default);
-	border-radius: 1em;
-	display: flex;
-	flex-direction: row;
-	justify-content: flex-start;
-	align-items: center;
-	gap: 0.6em;
-
-	&:hover {
-		color: var(--color-text-light-hover);
-		background-color: var(--color-sidebar-background-light-hover);
-	}
-
-	&:active {
-		color: var(--color-text-light-active);
-		background-color: var(--color-button-background-light-active);
-	}
-`;
-
 const Sidebar = (props) => {
     const { color } = props;
     const [isOpened, setIsOpened] = useState(true);
     const containerClassnames = classnames('sidebar', { opened: isOpened });
-
-    const goToRoute = (path) => {
-        console.log(`going to "${path}"`);
-    };
 
     const toggleSidebar = () => {
         setIsOpened(v => !v);
@@ -118,9 +68,6 @@ const Sidebar = (props) => {
 
     return (
         <StyledSidebar className={ containerClassnames }>
-
-			{/* <Title /> */}
-
             <StyledLogoWrapper>
                 <StyledLogo src={ logo } alt="TensorFlow logo"/>
 
@@ -131,37 +78,7 @@ const Sidebar = (props) => {
                 </StyledToggle>
             </StyledLogoWrapper>
 
-            <StyledRoutesWrapper>
-                {
-                    routes.map(route => (
-                        <StyledRoute
-                            key={ route.title }
-                            onClick={() => {
-                                goToRoute(route.path);
-                            }}
-                        >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span>{ route.title }</span>
-                        </StyledRoute>
-                    ))
-                }
-            </StyledRoutesWrapper>
-
-            <StyledRoutesWrapper>
-                {
-                    bottomRoutes.map(route => (
-                        <StyledRoute
-                            key={ route.title }
-                            onClick={() => {
-                                goToRoute(route.path);
-                            }}
-                        >
-                            <FontAwesomeIcon icon={ route.icon }/>
-                            <span>{ route.title }</span>
-                        </StyledRoute>
-                    ))
-                }
-            </StyledRoutesWrapper>
+			<Routes />
         </StyledSidebar>
     );
 };
