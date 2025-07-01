0a9bfa63f3fbab0de932141beff373457afdad51
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-07-01 11:43:27 +0300

    Add StylesAppWrapper, toggleTheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import Sidebar from "./components/Sidebar";
+import styled from 'styled-components'
 
 library.add(fas);
 
+const StyledAppWrapper = styled.div`
+	padding: 70px;
+	background-color: var(--color-background-light-default);
+	color: var(--color-text-light-default);
+`;
+
 const App = () => {
+
+	const [theme, setTheme] = useState('light');
+
+	const toggleTheme = () => {
+		setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
+	}
+
     return (
-        <Sidebar color='light' />
+		<StyledAppWrapper>
+			<Sidebar color={theme} onToggleTheme = {toggleTheme} />
+		</StyledAppWrapper>
     )
 }
 
